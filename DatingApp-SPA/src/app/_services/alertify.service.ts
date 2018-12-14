ad56ac9ceb435ca1f7510d81614f1f2a3f6c2bbe
buddhika85 <buddhika2006system@gmail.com>
2018-12-14 19:56:40 +0530

    added 3rd party client side libraries

---
+++ DatingApp-SPA/src/app/_services/alertify.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+declare let alertify: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertifyService {
+
+constructor() { }
+
+confirm(message: string, okCallback: () => any){
+  alertify.confirm(message, function(e) {
+    if(e) {
+      okCallback();
+    } else {}
+  });
+}
+}
