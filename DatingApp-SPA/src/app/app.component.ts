2a7a986d0c7a280070d2e37410ad947223fc0f66
buddhika85 <buddhika2006system@gmail.com>
2018-12-24 11:44:20 +0530

    Profiles - End of section 9

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -7,14 +7,14 @@ import { JwtHelperService } from '@auth0/angular-jwt';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit { 
+export class AppComponent implements OnInit {
   jwtHelper = new JwtHelperService();
 
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
     const token = localStorage.getItem('token');
-    if (token){
+    if (token) {
       this.authService.decodedToken = this.jwtHelper.decodeToken(token);
     }
   }
